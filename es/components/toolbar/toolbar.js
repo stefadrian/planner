55476ded1a5f09848264849bc2d3791de1afe626
TimoteiExt <timotei.bondas@extendstudio.com>
2023-08-01 10:34:32 +0300

    2.0.25

--- es/components/toolbar/toolbar.js
+++ es/components/toolbar/toolbar.js
@@ -117,105 +117,202 @@ var Toolbar = function (_Component) {
       var alterateColor = alterate ? SharedStyle.MATERIAL_COLORS[500].orange : "";
 
       var sorter = [
-      /*{
-        index: 0,
-        condition: true,
-        dom: (
-          <ToolbarButton
-            active={false}
-            tooltip={"Add Wall"}
-            onClick={(event) => {
-              projectActions.pushLastSelectedCatalogElementToHistory({ name: 'wall' });
-              linesActions.selectToolDrawingLine('wall');
-            }}
-          >
-            <FaPallet />
-          </ToolbarButton>
-        ),
-      },
-      {
-        index: 1,
-        condition: true,
-        dom: (
-          <ToolbarButton
-            active={false}
-            tooltip={"Add Door"}
-            onClick={(event) => {
-              holesActions.selectToolDrawingHole('door');
-              projectActions.pushLastSelectedCatalogElementToHistory({ name: 'door' });
-            }}
-          >
-            <FaDoorOpen />
-          </ToolbarButton>
-        ),
-      },
-      {
-        index: 2,
-        condition: true,
-        dom: (
-          <ToolbarButton
-            active={false}
-            tooltip={"Add Window"}
-            onClick={(event) => {
-              holesActions.selectToolDrawingHole('window');
-              projectActions.pushLastSelectedCatalogElementToHistory({ name: 'window' });
-            }}
-          >
-            <FaWindows />
-          </ToolbarButton>
-        ),
-      },*/
-      // {
-      //   index: 19,
-      //   condition: true,
-      //   dom: (
-      //     <ToolbarButton
-      //       active={[MODE_VIEWING_CATALOG].includes(mode)}
-      //       tooltip={translator.t("Open catalog")}
-      //       onClick={(event) => projectActions.openCatalog()}
-      //     >
-      //       <FaPlus />
-      //     </ToolbarButton>
-      //   ),
-      // },
-      {
-        index: 20,
-        condition: true,
-        dom: React.createElement(
-          ToolbarButton,
-          {
-            active: false,
-            tooltip: translator.t("Undo (CTRL-Z)"),
-            onClick: function onClick(event) {
-              return projectActions.undo();
-            }
-          },
-          React.createElement(MdUndo, null)
-        )
-      }];
+        /*{
+          index: 0,
+          condition: true,
+          dom: (
+            <ToolbarButton
+              active={false}
+              tooltip={"Add Wall"}
+              onClick={(event) => {
+                projectActions.pushLastSelectedCatalogElementToHistory({ name: 'wall' });
+                linesActions.selectToolDrawingLine('wall');
+              }}
+            >
+              <FaPallet />
+            </ToolbarButton>
+          ),
+        },
+        {
+          index: 1,
+          condition: true,
+          dom: (
+            <ToolbarButton
+              active={false}
+              tooltip={"Add Door"}
+              onClick={(event) => {
+                holesActions.selectToolDrawingHole('door');
+                projectActions.pushLastSelectedCatalogElementToHistory({ name: 'door' });
+              }}
+            >
+              <FaDoorOpen />
+            </ToolbarButton>
+          ),
+        },
+        {
+          index: 2,
+          condition: true,
+          dom: (
+            <ToolbarButton
+              active={false}
+              tooltip={"Add Window"}
+              onClick={(event) => {
+                holesActions.selectToolDrawingHole('window');
+                projectActions.pushLastSelectedCatalogElementToHistory({ name: 'window' });
+              }}
+            >
+              <FaWindows />
+            </ToolbarButton>
+          ),
+        },*/
+        // {
+        //   index: 19,
+        //   condition: true,
+        //   dom: (
+        //     <ToolbarButton
+        //       active={[MODE_VIEWING_CATALOG].includes(mode)}
+        //       tooltip={translator.t("Open catalog")}
+        //       onClick={(event) => projectActions.openCatalog()}
+        //     >
+        //       <FaPlus />
+        //     </ToolbarButton>
+        //   ),
+        // },
+        // {
+        //   index: 20,
+        //   condition: true,
+        //   dom: (
+        //     <ToolbarButton
+        //       active={false}
+        //       tooltip={translator.t("Undo (CTRL-Z)")}
+        //       onClick={(event) => projectActions.undo()}
+        //     >
+        //       <MdUndo />
+        //     </ToolbarButton>
+        //   ),
+        // },
+        // {
+        //   index: 0,
+        //   condition: allowProjectFileSupport,
+        //   dom: (
+        //     <ToolbarButton
+        //       active={false}
+        //       tooltip={translator.t("New project")}
+        //       onClick={(event) =>
+        //         confirm(translator.t("Would you want to start a new Project?"))
+        //           ? projectActions.newProject()
+        //           : null
+        //       }
+        //     >
+        //       <FaFile />
+        //     </ToolbarButton>
+        //   ),
+        // },
+        // {
+        //   index: 1,
+        //   condition: allowProjectFileSupport,
+        //   dom: (
+        //     <ToolbarSaveButton
+        //       state={state}
+        //       onSaveAction={this.props.onSaveAction}
+        //     />
+        //   ),
+        // },
+        // {
+        //   index: 2,
+        //   condition: allowProjectFileSupport,
+        //   dom: <ToolbarLoadButton state={state} />,
+        // },
+        // {
+        //   index: 3,
+        //   condition: true,
+        //   dom: (
+        //     <ToolbarButton
+        //       active={[MODE_VIEWING_CATALOG].includes(mode)}
+        //       tooltip={translator.t("Open catalog")}
+        //       onClick={(event) => projectActions.openCatalog()}
+        //     >
+        //       <FaPlus />
+        //     </ToolbarButton>
+        //   ),
+        // },
+        // {
+        //   index: 4,
+        //   condition: true,
+        //   dom: (
+        //     <ToolbarButton
+        //       active={[MODE_3D_VIEW].includes(mode)}
+        //       tooltip={translator.t("3D View")}
+        //       onClick={(event) => viewer3DActions.selectTool3DView()}
+        //     >
+        //       <Icon3D />
+        //     </ToolbarButton>
+        //   ),
+        // },
+        // {
+        //   index: 5,
+        //   condition: true,
+        //   dom: (
+        //     <ToolbarButton
+        //       active={[MODE_IDLE].includes(mode)}
+        //       tooltip={translator.t("2D View")}
+        //       onClick={(event) => projectActions.setMode(MODE_IDLE)}
+        //     >
+        //       {[MODE_3D_FIRST_PERSON, MODE_3D_VIEW].includes(mode) ? (
+        //         <Icon2D style={{ color: alterateColor }} />
+        //       ) : (
+        //         <FaMousePointer style={{ color: alterateColor }} />
+        //       )}
+        //     </ToolbarButton>
+        //   ),
+        // },
+        // {
+        //   index: 6, condition: true, dom: <ToolbarButton
+        //     active={[MODE_3D_FIRST_PERSON].includes(mode)}
+        //     tooltip={translator.t('3D First Person')}
+        //     onClick={event => viewer3DActions.selectTool3DFirstPerson()}>
+        //     <MdDirectionsRun />
+        //   </ToolbarButton>
+        // },
+        // {
+        //   index: 8,
+        //   condition: true,
+        //   dom: (
+        //     <ToolbarButton
+        //       active={[MODE_CONFIGURING_PROJECT].includes(mode)}
+        //       tooltip={translator.t("Configure project")}
+        //       onClick={(event) => projectActions.openProjectConfigurator()}
+        //     >
+        //       <MdSettings />
+        //     </ToolbarButton>
+        //   ),
+        // },
+      ];
 
       var style = toolbarProps.orientation === 'horizontal' ? TOP_STYLE : ASIDE_STYLE;
       var addSorter = [];
 
       var index = 0;
-      toolbarProps.buttons.map(function (element) {
-        addSorter.push({
-          index: index,
-          condition: element.condition,
-          dom: React.createElement(
-            ToolbarButton,
-            {
-              active: false,
-              tooltip: element.tooltip,
-              onClick: function onClick() {
-                return element.onClickEvent(projectActions, itemsActions, linesActions, holesActions);
-              }
-            },
-            element.iconData
-          )
+      if (toolbarProps && toolbarProps.buttons && toolbarProps.buttons.length > 0) {
+        toolbarProps.buttons.map(function (element) {
+          addSorter.push({
+            index: index,
+            condition: element.condition,
+            dom: React.createElement(
+              ToolbarButton,
+              {
+                active: false,
+                tooltip: element.tooltip,
+                onClick: function onClick() {
+                  return element.onClickEvent(projectActions, itemsActions, linesActions, holesActions);
+                }
+              },
+              element.iconData
+            )
+          });
+          index++;
         });
-        index++;
-      });
+      }
 
       var finalSorter = addSorter.concat(sorter);
 
